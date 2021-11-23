3fa1ca2b58cea662d75d12b6cb22b6b0b33e8626
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2021-11-23 22:08:30 +0000

    main added

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from ads_scrapper_indeed import ads_scrapper_indeed
 keyword = "Data Analyst"
 all_links = links_scrapper(keyword, location="United Kingdom", num_pages=1)
 ads_db = []
-ads_db.append(ads_scrapper_indeed(all_links[0], keyword))
+for index, link in enumerate(all_links):
+    print(str(index + 1) + " of " + str(len(all_links)))
+    ads_db.append(ads_scrapper_indeed(all_links[index], keyword))
